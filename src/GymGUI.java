572845430d7a38703d15b5f508decb8f7fed8389
BibekPoudel855 <bibekpoudel855@gmail.com>
2025-03-05 22:26:59 +0545

    added array list in gui class

--- src/GymGUI.java
+++ src/GymGUI.java
@@ -1,7 +1,7 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 public class GymGUI {
-
     public static void main(String[] args) {
-
+        ArrayList memberDetailList = new ArrayList();
     }
 }
